9701dd50f151c5b7bf9fb21954ab27e01b42e7d1
talestsp <tales.tsp@gmail.com>
2015-01-24 17:07:47 -0300

    Projeto para recuperar as coordenadas Lat e Lng dada uma rua.

---
+++ RuaToLatLog/src/crawling/StreetMining.java
@@ -0,0 +1,60 @@
+package crawling;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class StreetMining {
+
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		
+		String html = coletar("av floriano peixoto campina grande pb");
+		
+		System.out.println();
+		System.out.println(">>>");
+		System.out.println(extrairLatLon(html));
+		
+	}
+	
+	
+	private static String extrairLatLon(String html) {
+		String[] location = html.split("<location>");
+		
+		String loc = location[1].split("</location>")[0];
+		
+		String lat = loc.split("<lat>")[1].split("</lat>")[0];
+		String lon = loc.split("<lng>")[1].split("</lng>")[0];
+		
+		lat = lat.trim();
+		lon = lon.trim();
+		
+		return "(" + lat + "," + lon + ")";
+	}
+
+
+
+
+	private static String coletar(String logradouro) throws Exception {		
+		
+		String url = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + logradouro.replace(" ", "+") + "&sensor=false";
+		
+		try {
+			Document doc = Jsoup.connect(url).get();
+			
+			System.out.println(doc.html());
+			
+			return doc.html();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		throw new Exception("Problemas na coleta");
+
+	}
+
+}
