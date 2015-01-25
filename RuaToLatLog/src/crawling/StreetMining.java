a2ad5f86ce430c80de371c2dedfa9ad6acfc8e32
arthurSena <arthur.senaufcg@gmail.com>
2015-01-24 22:01:29 -0500

    Ruas quase concertada

--- RuaToLatLog/src/crawling/StreetMining.java
+++ RuaToLatLog/src/crawling/StreetMining.java
@@ -1,8 +1,15 @@
 package crawling;
 
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 public class StreetMining {
 
@@ -12,18 +19,54 @@ public class StreetMining {
 	public static void main(String[] args) throws Exception {
 		
 		
-		String xml = coletar("av floriano peixoto", "campina grande pb");
 		
-		System.out.println();
-		System.out.println(">>>");
-		System.out.println(extrairLatLon(xml));
+		try{
+			HashMap<String, String> mapa = new HashMap<String, String>();
+			String csvFilename = "/home/arthur/Documents/SafeBus2/crimes_considerados.csv";
+			String outputFile = "/home/arthur/Documents/SafeBus2/outPut.csv";
+			CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+			CSVWriter writer = new CSVWriter(new FileWriter(outputFile));
+			String[] row = null;
+			int count = 0;
+			while((row = csvReader.readNext()) != null) {
+				String lougadoro = row[12];
+				if (count !=0){
+					if(!lougadoro.isEmpty()){
+						if (!mapa.containsKey(lougadoro)){
+							mapa.put(lougadoro, coletar(lougadoro, "campina grande pb"));
+						}
+						String lat = mapa.get(lougadoro).split(",")[0];
+						String longi = mapa.get(lougadoro).split(",")[1];
+
+						String[] temp = {lougadoro, lat,longi};
+						System.out.println(temp[0] + "/" + temp[1] + "/" + temp[2]);
+						writer.writeNext(temp);
+						
+					}
+				}
+				count++;
+			}
+			writer.close();
+			csvReader.close();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		
+		}
+		
+		
+		
+//		String xml = coletar("av floriano peixoto", "campina grande pb");
+		
+//		System.out.println();
+//		System.out.println(">>>");
+//		System.out.println(extrairLatLon(xml));
 		
 	}
 	
 	
 	private static String extrairLatLon(String html) {
 		String[] location = html.split("<location>");
-		
+		System.out.println("Location: "+ location[0]);
 		String loc = location[1].split("</location>")[0];
 		
 		String lat = loc.split("<lat>")[1].split("</lat>")[0];
@@ -32,29 +75,36 @@ public class StreetMining {
 		lat = lat.trim();
 		lon = lon.trim();
 		
-		return "(" + lat + "," + lon + ")";
+		return  lat + "," + lon;
 	}
 
 
 
 
 	private static String coletar(String logradouro, String cidade) throws Exception {		
-		
-		String url = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + logradouro.replace(" ", "+") + "+" + cidade.replace(" ", "+") + "&sensor=false";
-		
-		try {
-			Document doc = Jsoup.connect(url).get();
-			
-			System.out.println(doc.html());
-			
-			return doc.html();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (logradouro.isEmpty()){
+			return "";
 		}
-		
-		throw new Exception("Problemas na coleta");
-
+		else{
+			if (logradouro.contains("RUA RIO BRANCO")){
+				logradouro = "Av. Barão Rio Branco";
+			}
+			else if(logradouro.contains("RUA MONSENHOR JOSE COUTINHO")){
+				logradouro = "Severino Pereira Rodrigues";
+			}
+			String url = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + logradouro.replace(" ", "+") + "+" + cidade.replace(" ", "+") + "&sensor=false";
+			
+			try {
+				Document doc = Jsoup.connect(url).get();
+				return extrairLatLon(doc.html());
+				
+			} catch (Exception e) {
+				System.out.println("Logradouro: "+logradouro);
+				e.printStackTrace();
+			}
+			
+			throw new Exception("Problemas na coleta");
+		}
 	}
 
 }
